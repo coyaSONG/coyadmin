9fd41d1b2a914bf82d9b8276a96b4022d7dca1ce
coyaSONG <qawsy12@gmail.com>
2024-04-07 03:54:00 +0900

    WIP: node_env condition add

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-const base = process.env.VITE_BASE_URL || '/';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: base,
+  base: process.env.NODE_ENV === 'development' ? '/' : '/coyadmin/',
   plugins: [react()],
 });
